ec88d5e52833a1e5a0238c8a8d0e983e6e6af3e4
kogoon <gjs712@naver.com>
2020-03-24 11:07:23 +0900

    start make file

---
+++ JS_IRScript.cmd
@@ -0,0 +1,51 @@
+:: IR (Incident Response) Script
+:: Written by : Kogoon
+:: For Univ. Assignment
+::
+:: 2020.03.24 start
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+:: Change (Update)
+::
+::
+::
+::
+::
+:: Don'show each command - @echo off
+@ECHO OFF
+rem Color 0A
+title K I R
+
+::
+::
+SET DATE = 
+SET LOG=.\log.txt
+
+SET /p MYNAME= Enter your name:
+echo %MYNAME%
+
+:: Start Log File
+ECHO KORKIR is running...
+>> %LOG% ECHO               Kogoon Incident Response
+>> %LOG% ECHO.
+>> %LOG% ECHO      Destination :    %LOG%
+>> %LOG% ECHO             Name :    %MYNAME%
+>> %LOG% ECHO    Computer Name :    %HOST%
+>> %LOG% ECHO               OS :    %VERSION%
+>> %LOG% ECHO.
+>> %LOG% ECHO START -- Date: %DATE% / Time: %TIME%
+>> %LOG% ECHO.
+CALL lines.bat ENDLINE
+ECHO %COMPUTERNAME%
+ECHO %VERSION%
+CALL lines.bat ENDLINE
+>> %LOG% ECHO.
+>> %LOG% ECHO   END -- Date: %DATE% / Time: %TIME%
